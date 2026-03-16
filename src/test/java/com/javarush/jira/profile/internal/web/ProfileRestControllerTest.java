e93cd910d99edf4107370ae377a7f9d35a88492e
kostkhar <konstantin.kharik@onlinesup.com>
2026-03-16 19:59:45 +0300

    add ProfileRestControllerTest

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,38 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.login.AuthUser;
 
+import com.javarush.jira.profile.internal.ProfileRepository;
+import com.javarush.jira.profile.internal.model.Profile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(ProfileRepository.class)
 class ProfileRestControllerTest extends AbstractControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ProfileRepository profileRepository;
+
+    @Test
+    void getProfileByAuthUser(){
+        Long profileId = 1L;
+        Profile profile = new Profile(profileId);
+        when(profileRepository.findById(profileId)).thenReturn(Optional.of(profile));
+
+    }
+
+
+
 
 }
