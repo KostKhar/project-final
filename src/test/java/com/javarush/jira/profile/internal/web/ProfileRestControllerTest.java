919d9fd9645951a1cd827112cad213dfa4d0b431
kostkhar <konstantin.kharik@onlinesup.com>
2026-03-18 19:32:54 +0300

    add ProfileRestControllerTest

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,22 +1,29 @@
 package com.javarush.jira.profile.internal.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javarush.jira.AbstractControllerTest;
 import com.javarush.jira.login.AuthUser;
 import com.javarush.jira.login.Role;
 import com.javarush.jira.login.User;
+import com.javarush.jira.profile.ContactTo;
 import com.javarush.jira.profile.ProfileTo;
 import com.javarush.jira.profile.internal.ProfileMapper;
 import com.javarush.jira.profile.internal.ProfileRepository;
 import com.javarush.jira.profile.internal.model.Profile;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 
 import java.util.List;
+import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,12 +31,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 class ProfileRestControllerTest extends AbstractControllerTest {
 
-
     @MockBean
     private ProfileRepository profileRepository;
 
     @MockBean
     private ProfileMapper profileMapper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void getProfileByAuthUser() throws Exception {
@@ -55,6 +64,9 @@ class ProfileRestControllerTest extends AbstractControllerTest {
                         .with(SecurityMockMvcRequestPostProcessors.authentication(auth)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1L));
+        verify(profileRepository).getOrCreate(authUser.id());
+        verify(profileMapper).updateFromTo(any(Profile.class), any(ProfileTo.class));
+        verify(profileRepository).save(any(Profile.class));
     }
 
     @Test
@@ -75,12 +87,44 @@ class ProfileRestControllerTest extends AbstractControllerTest {
         perform(get("/api/profile"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.title").value("Unauthorized"))
-                .andExpect(jsonPath("$.detail").value("Full authentication is required to access this resource"))
                 .andExpect(jsonPath("$.status").value(HttpStatus.UNAUTHORIZED.value()));
     }
 
     @Test
-    void updateProfile() throws Exception {
+    void checkUserUpdateProfileWithoutAuth() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        user.setDisplayName("John Doe");
+        user.setEmail("john@example.com");
+        user.setPassword("password");
+        user.setRoles(List.of(Role.DEV));
+
+
+        User admin = new User();
+        admin.setId(5L);
+        user.setFirstName("Tom");
+        user.setLastName("Khan");
+        user.setDisplayName("Tom Khan");
+        user.setEmail("admin@example.com");
+        user.setPassword("password");
+        user.setRoles(List.of(Role.ADMIN));
+
+
+        ProfileTo profileTo = new ProfileTo(2L, Set.of("google"), Set.of(new ContactTo("1111", "neworg@gmail.com")));
+
+        String jsonBody = new ObjectMapper().writeValueAsString(profileTo);
+
+        perform(put("/api/profile")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.title").value("Unauthorized"));
+    }
+
+    @Test
+    void checkAdminUpdateProfile() throws Exception {
         User user = new User();
         user.setId(1L);
         user.setFirstName("John");
@@ -110,12 +154,22 @@ class ProfileRestControllerTest extends AbstractControllerTest {
 
         ProfileTo profileTo = new ProfileTo(2L, null, null);
 
+        String jsonBody = objectMapper.writeValueAsString(profileTo);
+
         when(profileRepository.getOrCreate(user.id())).thenReturn(profile);
         when(profileMapper.updateFromTo(profileRepository.getOrCreate(profileTo.id()), profileTo)).thenReturn(profile);
 
-        perform(put("/api/profile").with(SecurityMockMvcRequestPostProcessors.authentication(auth)).param("firstName", "John"));
+        perform(put("/api/profile")
+                .with(SecurityMockMvcRequestPostProcessors.authentication(auth))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonBody))
+                .andExpect(status().isNoContent());
 
+        verify(profileRepository).getOrCreate(user.id());
+        verify(profileMapper).updateFromTo(profileRepository.getOrCreate(profileTo.id()), profileTo);
+        verify(profileRepository).save(profile);
     }
+
 
 
 
